53ed05cc16d50270c5319b8f5210f761c2d10622
Jemimah_Mwihava <semahalc@gmail.com>
2020-06-28 15:28:29 +0300

    Added the Hero and Banner components

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,9 @@
-import React from 'react'
+import React from 'react';
+import Hero from "../components/Hero";
 
 export const Home = () => {
     return (
-        <div>
-            Hello from Home page.
-        </div>
+        <Hero />
     )
 }
 
